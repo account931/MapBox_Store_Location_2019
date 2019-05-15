f63a1e59b6d7c33e644290af1d2f4f10ec4c0dc6
Dima <account931@ukr.net>
2019-05-15 15:50:15 +0300

    15MAy_19-> implemented adding/deleting a marker = TRUE

--- Credentials/api_access_token.js
+++ Credentials/api_access_token.js
@@ -1,9 +1,9 @@
 //MApBox Api access token
 //pk.eyJ
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWNjb3VudDkzMSIsImEiOiJjaXgwOTVuOTEwMGFxMnVsczRwOWx0czhnIn0.YjZ5bpnh6jqTEk7cCJfrzw';
+mapboxgl.accessToken = 'pk.';
 
 
-mapboxgl.accessTokenSecret = 'sk.eyJ1IjoiYWNjb3VudDkzMSIsImEiOiJjanY2b2hlZGcwMWhiM3lueDdwbzZzOWF3In0.hqc2LxK4nIfPfI7t2iN8Sg';
+mapboxgl.accessTokenSecret = 'sk.';
 
 //var access_tokennn_from_credentials = 'pk.eyJ';
 
